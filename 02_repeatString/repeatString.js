3e1473aaed839905135ea4511e1307fdecb48d54
Paul Justine <111945104+justpaulmiguel@users.noreply.github.com>
2024-03-07 16:53:36 +0800

    solve the activities from 1 to 7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let string1 = "";
+  for (let i = num; i > 0; i--) {
+    string1 += string;
+  }
+  if (num < 0) return "ERROR";
+  else return string1;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num1) {
+    const array1 =[];
 
+    array.forEach(numbers => {
+        if(!num1.includes(numbers)){
+            array1.push(numbers);
+        }
+    });
+    return array1;
 };
 
 // Do not edit below this line
