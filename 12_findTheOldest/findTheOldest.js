a3333f369454ec30e0d3cccc0d16213e504f528f
Paul Justine <111945104+justpaulmiguel@users.noreply.github.com>
2024-03-15 17:15:26 +0800

    do javascript-exercises 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const oldest = people.sort((currPerson, nextPerson) => {
+    const aAge = getAge(currPerson.yearOfBirth, currPerson.yearOfDeath);
+    const bAge = getAge(nextPerson.yearOfBirth, nextPerson.yearOfDeath);
+    return aAge > bAge ? -1 : 1;
+  });
+  // console.log(oldest[0]);
+  return oldest[0];
+};
 
+const getAge = (birth, death) => {
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
