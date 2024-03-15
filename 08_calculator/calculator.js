1497b609b1020d1d5c94740767261258f9535b9d
Paul Justine <111945104+justpaulmiguel@users.noreply.github.com>
2024-03-15 15:08:26 +0800

    do javascript-exercises 8_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(num1) {
+  return num1.reduce((total, curr)=> total+=curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(num1) {
+  return num1.reduce((total, curr)=> total*=curr, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return Math.pow(num1, num2)
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  let num2 = 1;
+  for(i = 1; i <= num1; i++){
+    num2*=i;
+  }
+  return num2;
 };
 
 // Do not edit below this line
