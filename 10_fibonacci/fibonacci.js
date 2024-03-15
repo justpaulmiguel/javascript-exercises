b14debd57aa273ccd5b4460d5a3e7ff5f556609e
Paul Justine <111945104+justpaulmiguel@users.noreply.github.com>
2024-03-15 15:57:39 +0800

    do javascript-exercises 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    let num1 = 0, num2 = 1, temp =0;
+    if(num <0){
+        return 'OOPS';
+    }
+    else{
+        for (i = 0; i <= num; i++) {
+      temp = num1;
+      num1 = num2;
+      num2 = temp;
+      num1 += num2;
+    } 
+    }
+    return num2;
 };
 
 // Do not edit below this line
