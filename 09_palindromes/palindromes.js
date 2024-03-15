7875aad051b3e073d5f8c4195f7e056820ca1240
Paul Justine <111945104+justpaulmiguel@users.noreply.github.com>
2024-03-15 15:37:13 +0800

    do javascript-exercises 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const alphanumeric = "abcdefghijklmnopqrstuvwxyz1234567890";
+  const clean = string
+    .toLowerCase()
+    .split("")
+    .filter((character) => alphanumeric.includes(character))
+    .join("");
 
+  let string1 = "";
+  for (i = 1; i <= clean.length; i++) {
+    string1 += clean[clean.length - i];
+  }
+  console.log(clean);
+  return string1 === clean;
 };
 
 // Do not edit below this line
